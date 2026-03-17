57494142518c0ab06be23a434217e35ade8fd401
heiljj <heiljj@rose-hulman.edu>
2026-03-17 16:02:57 -0500

    decreased flow tap, removed flow tap on sft keys

--- keyboards/keebio/iris_lm/keymaps/3x5/keymap.c
+++ keyboards/keebio/iris_lm/keymaps/3x5/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_NO,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   KC_NO,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_NO,LSFT_T(KC_A),LCTL_T(KC_S),LALT_T(KC_D), KC_F,KC_G,                         KC_H,    KC_J,    RALT_T(KC_K),RCTL_T(KC_L),LSFT_T(KC_SCLN), KC_NO,
+     KC_NO,LSFT_T(KC_A),LCTL_T(KC_S),LALT_T(KC_D), KC_F,KC_G,                         KC_H,    KC_J,    RALT_T(KC_K),RCTL_T(KC_L),RSFT_T(KC_SCLN), KC_NO,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_NO,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_NO,             KC_NO,   KC_N,    KC_M,  KC_COMM,  KC_DOT, KC_SLSH,  KC_NO,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
@@ -148,4 +148,13 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 void keyboard_post_init_user(void) {
     rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
     rgb_matrix_sethsv_noeeprom(HSV_OFF);
+}
+
+uint16_t get_flow_tap_term(uint16_t keycode, keyrecord_t* record, uint16_t prev_keycode) {
+    switch (keycode) {
+        case LSFT_T(KC_A): return 75;
+        case RSFT_T(KC_SCLN): return 75;
+        default: return 200;
+    }
+    return 0;
 }
